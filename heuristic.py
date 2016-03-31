9533384084dc066cd475fe60075ef1a25a90f446
kenliao94 <kenliao94@gmail.com>
2016-03-30 21:54:55 -0400

    Add heuristic file for heuristic

---
+++ heuristic.py
@@ -0,0 +1,32 @@
+def heuristic_1(board):
+    '''
+    :param board: A board object
+    :return: a heuristic value
+    '''
+    # specify the algorithm
+    return
+
+def heuristic_2(board):
+    '''
+    :param board: A board object
+    :return: a heuristic value
+    '''
+    # specify the algorithm
+    return
+
+def heuristic_3(board):
+    '''
+    :param board: A board object
+    :return: a heuristic value
+    '''
+    # specify the algorithm
+    return
+
+def heuristic_4(board):
+    '''
+    :param board: A board object
+    :return: a heuristic value
+    '''
+    # specify the algorithm
+    return
+
