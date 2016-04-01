ae3b2d7411c1c25d7c9f527e1508e28dd0da3cd8
kenliao94 <kenliao94@gmail.com>
2016-04-01 15:31:55 -0400

    Added Minimax with alpha beta pruning

--- gametree.py
+++ gametree.py
@@ -29,7 +29,6 @@ def minimax(node,current_level,search_limit,heur,player,max_player,min_player):
     #check if the depth limit is reached
     if current_level >= search_limit:
         return heur(node.board,node,player)
-
     #general cases
     #refer to slides, also use deepcopy to create node
     score_array = []
@@ -52,4 +51,40 @@ def minimax(node,current_level,search_limit,heur,player,max_player,min_player):
         return  min(score_array)
     else:
         raise  NameError("Unexpected case at last in minimax")
-    
+
+def alphabeta_minimax(node,current_level,search_limit,heur,player,max_player,min_player,alpha,beta):
+    #base cases for recursion, i.e check if depth limit is reached or the game is ended
+        #Case 1 check if game is ended
+    if node.board.check_end_game():
+        winner = node.board.get_winner()
+        if winner == Piece.TIE:
+            return Constant.tie_value
+        elif winner == node.max_player:
+            return Constant.max_player_endgame_value
+        elif winner == node.min_player:
+            return Constant.min_player_endgame_value
+        else:
+            raise NameError("Unexpected case at minimax check phase")
+    #check if the depth limit is reached
+    if current_level >= search_limit:
+        return heur(node.board,node,player)
+    #general cases
+    if player == max_player:
+        for possible_move in node.board.get_possible_move(player):
+            new_node = deepcopy(node)
+            new_node.board.make_move(possible_move,player)
+            alpha = max(alpha,alphabeta_minimax(new_node,current_level+1,search_limit,heur,min_player,max_player,min_player,alpha,beta))
+            if beta <= alpha:
+                break
+        return alpha
+    elif player == min_player:
+        for possible_move in node.board.get_possible_move(player):
+            new_node = deepcopy(node)
+            new_node.board.make_move(possible_move,player)
+            beta = min(beta,alphabeta_minimax(new_node,current_level+1,search_limit,heur,max_player,max_player,min_player,alpha,beta))
+            if beta <= alpha:
+                break
+        return beta
+    else:
+        raise NameError("Unexpected case at minimax alpha beta")
+    #LOOK At slides, not correctly implemented
